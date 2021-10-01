2a3a4045928630be188c41edb6eb9a945ae4250d
knight <knight.sarai.dev@gmail.com>
2021-10-01 22:31:39 +0300

    Create attributes model

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 import User from 'models/User';
 
 const user = new User({ id: 1 });
-
-user.events.on('change', () => console.log('User Change'));
-
-user.fetch();
-
-user.events.trigger('change');
