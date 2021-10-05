163174a7373c6556c56b6a3012c2b58ffa304f14
knight <knight.sarai.dev@gmail.com>
2021-10-05 21:25:59 +0300

    Add user for,

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import UserForm from 'views/UserForm';
+import User from 'models/User';
 
-const userForm = new UserForm(document.getElementById('root')!);
+const userForm = new UserForm(
+  document.getElementById('root')!,
+  User.create({ name: 'knight', age: 25 })
+);
 
 userForm.render();
--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,19 +1,60 @@
+import User from 'models/User';
+
 class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {
+    this.renderOnChange();
+  }
+
+  renderOnChange(): void {
+    this.model.on('change', () => this.render());
+  }
+
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:.age-button': this.onAgeButtonClick,
+    };
+  }
+
+  onAgeButtonClick = (): void => {
+    this.model.set({ age: Math.round(Math.random() * 100) });
+    console.log(this.model.get('age'));
+  };
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventMap = this.eventsMap();
+    for (let eventKey in eventMap) {
+      const [eventName, selector] = eventKey.split(':');
+      const elements = fragment.querySelectorAll(selector);
+      elements.forEach((element) => {
+        element.addEventListener(eventName, eventMap[eventKey]);
+      });
+    }
+  }
+
+  onButtonClick(): void {
+    console.log('Hi');
+  }
 
   template(): string {
     return `
             <div>
                 <h1>User Form</h1>
+                <div>
+                    <p>User Name: ${this.model.get('name')}</p>
+                    <p>User Age: ${this.model.get('age')}</p>
+                </div>
                 <input/>
                 <button>Click Me</button>
+                <button class="age-button">Set Age</button>
             </div>
         `;
   }
 
   render(): void {
+    this.parent.innerHTML = '';
     const tempateElement = document.createElement('template');
     tempateElement.innerHTML = this.template();
+    this.bindEvents(tempateElement.content);
     this.parent.append(tempateElement.content);
   }
 }
